65e9d6017ee7675718441b3fd5d5868d2c7d63af
Rahul <rahul@thinksys.com>
2025-01-22 15:23:58 +0530

    added one more case

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,17 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
+test('verify google title', async ({ page }) => {
   await page.goto('https://www.google.com');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Google/);
+});
+
+test('Verify facebook title', async ({ page }) => {
+  await page.goto('https://www.facebook.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Facebook – log in or sign up/);
 });
 
